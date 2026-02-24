ac74928855023d3146cf0988ac290c9d6a37c933
younslemt <80912372+younslemt@users.noreply.github.com>
2026-02-24 01:19:28 +0100

    Delete site/client/src directory

--- site/client/src/services/mockData.ts
+++
@@ -1,8 +0,0 @@
-import { User, Organization, Sprint, Task, Document, Diagnostic } from '../types';
-
-export const MOCK_USERS: User[] = [];
-export const MOCK_ORGANIZATIONS: Organization[] = [];
-export const MOCK_SPRINTS: Sprint[] = [];
-export const MOCK_TASKS: Task[] = [];
-export const MOCK_DOCUMENTS: Document[] = [];
-export const MOCK_DIAGNOSTICS: Diagnostic[] = [];
